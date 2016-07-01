7fe27327d7638f1197213c1ae1766020e36207c3
happy <joe.le@dianrong.com>
2016-07-01 15:11:59 +0800

    Support webpack-dev-server

---
+++ dev-server.js
@@ -0,0 +1,23 @@
+var WebpackDevServer = require('webpack-dev-server');
+var webpack = require('webpack');
+var config = require('./webpack.config.js');
+
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+  contentBase: './app',
+  hot: true,
+  historyApiFallback: false,
+  compress: true,
+  quiet: false,
+  noInfo: true,
+  lazy: false,
+  publicPath: '/dist/',
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  headers: { 'X-Custom-Header': 'yes' },
+  stats: { colors: true }
+});
+server.listen(1377, 'localhost', function() {});
+console.log('Access http://localhost:1377/ to run you app');
